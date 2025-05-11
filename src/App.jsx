d46ffce565e4c13515fedb1543e7c2d26c0e8afd
Anik Roy <anikroy.uiu@gmail.com>
2025-05-11 19:50:31 +0600

    Route setup& Components creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-import Home from './pages/Home'
+import Home from './pages/homepage/Home'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-
+import ProductList from './pages/productlist/ProductList'
+import ProductDetails from './pages/productdetails/ProductDetails'
+import BlogPage from './pages/blog/blogPage'
+import CheckOutPage from './pages/checkout/CheckOutPage'
+import ContactPage from './pages/contact/ContactPage'
 function App() {
-  
+   
 
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route index element={<Home />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/product-list" element={<ProductList />} />
+      <Route path="/product-details" element={<ProductDetails />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+     
     </Routes>
   )
 }
