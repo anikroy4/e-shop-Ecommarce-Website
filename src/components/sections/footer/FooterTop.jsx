8866d5e9392baf32bf9fb4a2b1704dc559214551
Anik Roy <anikroy.uiu@gmail.com>
2025-05-15 00:07:47 +0600

    changes of padding top

--- src/components/sections/footer/FooterTop.jsx
+++ src/components/sections/footer/FooterTop.jsx
@@ -5,7 +5,7 @@ import Button from '../../layers/Button'
 const FooterTop = () => {
   return (
     <Container className='bg-[linear-gradient(90deg,rgba(244,244,244,1)_29%,rgba(217,217,217,1)_43%)] px-25 py-28.5 rounded-[25px] '>
-      <h3 className='font-["Poppins"] font-semibold text-4xl pt-28.5'>
+      <h3 className='font-["Poppins"] font-semibold text-4xl pt-[103px]'>
         Get Our Updates
       </h3>
       <p className=' max-w-[475px] font-["Montserrat"] font-normal text-xl leading-[30px] mt-4 mb-5' >Browse our wide selection of electronics and find the perfect promo for you from newsletter.</p>
