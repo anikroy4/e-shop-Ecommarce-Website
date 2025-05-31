fa58f6bfd34aa132c4a8394d352041341ce8ad7e
Anik Roy <anikroy.uiu@gmail.com>
2025-05-31 09:19:27 +0600

    Update BandSelection.jsx

--- src/components/bandselection/BandSelection.jsx
+++ src/components/bandselection/BandSelection.jsx
@@ -25,19 +25,19 @@ const BandSelection = () => {
   return (
     <Container className='mt-[100px] mb-[100px] items-center '>
       <Slider {...settings} >
-        <div className="flex justify-center items-center mx-5">
+        <div className="flex justify-center items-center mr-[199px]">
           <SamsungIcon />
         </div>
-        <div className="flex justify-center items-center mx-5">
+        <div className="flex justify-center items-center mr-[199px]">
           <OpenAiIcon />
         </div>
-        <div className="flex justify-center items-center mx-5">
+        <div className="flex justify-center items-center mr-[199px]">
           <AmazonIcon />
         </div>
-        <div className="flex justify-center items-center mx-5">
+        <div className="flex justify-center items-center mr-[199PX]">
           <TancentIcon />
         </div>
-        <div className="flex justify-center items-center mx-5">
+        <div className="flex justify-center items-center mr-[199PX]">
           <SpotifyIcon />
         </div>
       </Slider>
