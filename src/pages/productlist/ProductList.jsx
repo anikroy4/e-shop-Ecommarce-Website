a8dbbedadb781edf1f470429f6ccfc5785c40b7b
Anik Roy <anikroy.uiu@gmail.com>
2025-06-15 23:21:42 +0600

    Update ProductList.jsx

--- src/pages/productlist/ProductList.jsx
+++ src/pages/productlist/ProductList.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Container from '../../components/layers/Container'
 
 const ProductList = () => {
   return (
-    <div> ProductList</div>
+    <Container>
+        <div className='flex'>
+          <div className="w-[355px] h-[200px] bg-[#F4F4F4] rounded-[25px]" >
+            <h1>Category</h1>
+            <div>item1</div>
+            <div>item1</div>
+            <div>item1</div>
+            <div>item1</div>
+          </div>
+          <div className="w">right</div>
+        </div>
+    </Container>
   )
 }
 
