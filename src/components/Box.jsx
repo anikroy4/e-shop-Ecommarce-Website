4a73fac199331ff90442df649e71e871f2d9cd09
Anik Roy <anikroy.uiu@gmail.com>
2025-08-07 14:30:13 +0600

    update Checkout page
    update Checkout page

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const Box = ({inputName, placeholder}) => {
+const Box = ({inputName, placeholder, type}) => {
   return (
     <>
-        <label className='font-["Poppins"] text-xl font-bold text-[#303030] leading-[30px] mb-3'>{inputName}<span className='text-red-600'>*</span>
-        <input type="text" className='w-full leading-[30px] border border-solid border-[#CBCBCB] rounded-[10px] px-8 py-[25px] outline-none font-["Montserrat"] font-normal ' placeholder={placeholder} />
-        </label>
+        <label className='font-["Poppins"] text-xl font-bold text-[#303030] leading-[30px] '>{inputName}<span className='text-red-600'>*</span></label>
+        { type=="textarea" ? 
+         <textarea className='w-full leading-[30px] border border-solid border-[#CBCBCB] rounded-[10px] px-8 py-[25px] outline-none font-["Montserrat"] font-normal  mt-3' placeholder={placeholder} />
+         :
+        <input type="text" className='w-full leading-[30px] border border-solid border-[#CBCBCB] rounded-[10px] px-8 py-[25px] outline-none font-["Montserrat"] font-normal mt-3 ' placeholder={placeholder} />
+        }
+        
+        
     </>
   )
 }
 
-export default Box
+export default Box 
--- src/pages/checkout/CheckOutPage.jsx
+++ src/pages/checkout/CheckOutPage.jsx
@@ -3,24 +3,69 @@ import Container from '../../components/layers/Container'
 import CheckOutBreadcrumb from '../../components/sections/checkout/CheckOutBreadcrumb'
 import Box from '../../components/Box'
 import { FaAngleDown } from 'react-icons/fa'
+import { BsTextarea } from 'react-icons/bs'
 
 const CheckOutPage = () => {
  let [active, setActive] = useState("Information")
   let [isOpen, setIsOpen] = useState(false)
+  let [isOpenState, setIsOpenState] = useState(false)
   let [selectedCountry, setSelectedCountry] = useState({ name: 'United Kingdom', language:'English', value: 'GB', flag: 'https://flagcdn.com/16x12/gb.png' });
+  let [selectedState, setselectedState] = useState('United Kingdom');
+
+  let [selectedCity, setSelectedCity] = useState('London');
+  let [isOpenCity, setIsOpenCity] = useState(false);
 
     const countries = [
-        {name: 'United Kingdom', language:'en', value: 'GB', flag: 'https://flagcdn.com/16x12/gb.png' },
-        {name: 'Bangladesh', language:'bd', value: 'BD', flag: 'https://flagcdn.com/16x12/bd.png' },
-        { name: 'Germany', language:'de', value: 'DE', flag: 'https://flagcdn.com/16x12/de.png' },
-        { name: 'France', language:'fr', value: 'FR', flag: 'https://flagcdn.com/16x12/fr.png' },
+        {name: 'United Kingdom', language:'en', value: 'GB', flag: 'https://flagcdn.com/16x12/gb.png',
+          states: ['England', 'Scotland', 'Wales', 'Northern Ireland'],
+          cities: ['London', 'Edinburgh', 'Cardiff', 'Belfast', 'Manchester', 'Birmingham', 'Glasgow', 'Liverpool', 'Leeds', 'Sheffield', 'Bristol', 'Newcastle upon Tyne', 'Nottingham', 'Southampton', 'Leicester', 'Coventry', 'Bradford', 'Brighton and Hove', 'Hull', 'Plymouth', 'Stoke-on-Trent', 'Derby', 'Swansea', 'Aberdeen', 'Dundee', 'Portsmouth', 'Wolverhampton', 'Luton', 'Reading', 'Blackpool', 'Bolton', 'Stockport', 'Walsall', 'Bournemouth', 'Southend-on-Sea', 'Swindon', 'Middlesbrough', 'Oxford', 'York', 'Cambridge', 'Milton Keynes', 'Peterborough', 'Norwich', 'Exeter', 'Cheltenham', 'Gloucester', 'Worcester', 'Hereford', 'Shrewsbury', 'Telford', 'St Albans', 'Watford', 'Basingstoke', 'Guildford', 'Crawley', 'Eastbourne'],
+        },
+        {name: 'Bangladesh', language:'bd', value: 'BD', flag: 'https://flagcdn.com/16x12/bd.png' ,
+          states: ['Dhaka', 'Chittagong', 'Khulna', 'Rajshahi', 'Sylhet'],
+          cities: ['Dhaka', 'Chittagong', 'Khulna', 'Rajshahi', 'Sylhet', 'Barisal', 'Rangpur', 'Mymensingh', 'Comilla', 'Narayanganj', 'Gazipur', 'Tangail', 'Bogra', 'Jessore', 'Noakhali', 'Pabna', 'Narsingdi', 'Cox\'s Bazar', 'Satkhira', 'Brahmanbaria', 'Kishoreganj', 'Manikganj', 'Patuakhali', 'Jhalokati', 'Bhola', 'Lalmonirhat', 'Dinajpur', 'Thakurgaon', 'Panchagarh', 'Nilphamari', 'Kurigram', 'Gaibandha', 'Joypurhat', 'Sherpur', 'Netrakona', 'Moulvibazar', 'Habiganj', 'Sunamganj', 'Sylhet Sadar', 'Rangamati', 'Bandarban', 'Khagrachari', 'Cox\'s Bazar Sadar', 'Feni', 'Lakshmipur', 
+          ],
+        },
+        { name: 'United States', language:'en', value: 'US', flag: 'https://flagcdn.com/16x12/us.png',
+          states: ['California', 'Texas', 'Florida', 'New York'],
+          cities: ['Los Angeles', 'Houston', 'Miami', 'New York City', 'Chicago', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Austin', 'Jacksonville', 'Fort Worth', 'Columbus', 'Charlotte', 'San Francisco', 'Indianapolis', 'Seattle', 'Denver', 'Washington D.C.', 'Boston', 'El Paso', 'Nashville', 'Detroit', 'Oklahoma City', 'Portland', 'Las Vegas', 'Louisville', 'Baltimore', 'Milwaukee', 'Albuquerque', 'Tucson', 'Fresno', 'Sacramento', 'Long Beach', 'Kansas City', 'Mesa', 'Virginia Beach', 'Atlanta', 'Colorado Springs', 'Omaha', 'Raleigh', 'Miami Gardens', 'Cleveland', 'Tulsa', 'Oakland', 'Minneapolis', 'Wichita', 'New Orleans', 'Arlington', 'Bakersfield', 'Tampa', 'Honolulu', 'Anaheim', 'Aurora', 'Santa Ana', 'St. Louis', 'Pittsburgh', 'Corpus Christi', 'Riverside', 'Cincinnati', 'Lexington', 'Anchorage', 'Stockton', 'Toledo', 'Saint Paul', 'Newark', 'Greensboro', 'Plano', 'Henderson', 'Lincoln', 'Buffalo', 'Fort Wayne', 'Jersey City', 'Chula Vista', 'Orlando', 'St. Petersburg', 'Chandler', 'Laredo', 'Madison', 'Durham', 'Lubbock', 'Garland', 'Glendale', 'Reno', 'Hialeah', 'Chesapeake', 'Scottsdale', 'North Las Vegas', 'Irving',
+          ],
+        },
+        { name: 'Canada', language:'en', value: 'CA', flag: 'https://flagcdn.com/16x12/ca.png',
+          states: ['Ontario', 'Quebec', 'British Columbia', 'Alberta'],
+          cities: ['Toronto', 'Montreal', 'Vancouver', 'Calgary', 'Edmonton', 'Ottawa', 'Winnipeg', 'Quebec City', 'Hamilton', 'Kitchener', 'London', 'Halifax', 'Victoria', 'Oshawa', 'Windsor', 'Saskatoon', 'Regina', 'St. John\'s', 'Barrie', 'Kelowna', 'Abbotsford', 'Sudbury', 'Sherbrooke', 'Guelph', 'Kingston', 'Moncton', 'Thunder Bay', 'Chicoutimi–Jonquière', 'Saint John', 'Nanaimo', 'Lethbridge', 'Kamloops', 'Peterborough', 'Sarnia', 'Brantford', 'Red Deer', 'Medicine Hat', 'Grande Prairie', 'Sault Ste. Marie', 'Prince George', 'Drummondville', 'Saint-Hyacinthe', 'Chatham-Kent', 'Fredericton', 'Cornwall', 'Belleville', 'Stratford', 'Moose Jaw', 'Lloydminster', 'Fort McMurray', 'Whitehorse', 'Yellowknife', 'Iqaluit', 'Corner Brook', 'Port Hawkesbury']
+        },
+        { name: 'Germany', language:'de', value: 'DE', flag: 'https://flagcdn.com/16x12/de.png',
+          states: ['Bavaria', 'Berlin', 'Hamburg', 'North Rhine-Westphalia'],
+          cities: ['Munich', 'Berlin', 'Hamburg', 'Cologne', 'Frankfurt', 'Stuttgart', 'Düsseldorf', 'Dortmund', 'Essen', 'Leipzig', 'Bremen', 'Dresden', 'Hannover', 'Nuremberg', 'Duisburg', 'Bochum', 'Wuppertal', 'Bielefeld', 'Mannheim', 'Bonn', 'Karlsruhe', 'Münster', 'Gelsenkirchen', 'Augsburg', 'Mönchengladbach', 'Braunschweig', 'Chemnitz', 'Kiel', 'Aachen', 'Magdeburg', 'Freiburg im Breisgau', 'Krefeld', 'Lübeck', 'Oberhausen', 'Erfurt', 'Mainz', 'Rostock', 'Hagen', 'Saarbrücken', 'Hamm', 'Mülheim an der Ruhr', 'Herne', 'Solingen', 'Osnabrück', 'Ludwigshafen am Rhein', 'Leverkusen', 'Oldenburg', 'Heidelberg', 'Potsdam'],
+        },
+        { name: 'Australia', language:'en', value: 'AU', flag: 'https://flagcdn.com/16x12/au.png' ,
+          states: ['New South Wales', 'Victoria', 'Queensland', 'Western Australia'],
+          cities: ['Sydney', 'Melbourne', 'Brisbane', 'Perth', 'Adelaide', 'Gold Coast', 'Canberra', 'Newcastle', 'Wollongong', 'Hobart', 'Geelong', 'Cairns', 'Townsville', 'Darwin', 'Toowoomba', 'Ballarat', 'Bendigo', 'Mackay', 'Coffs Harbour', 'Launceston', 'Albury-Wodonga', 'Wagga Wagga', 'Shepparton-Mooroopna', 'Sunshine Coast', 'Central Coast', 'Bunbury', 'Coffs Harbour', 'Mildura', 'Port Macquarie', 'Rockhampton', 'Tamworth', 'Bundaberg', 'Geraldton', 'Kalgoorlie-Boulder', 'Mount Gambier', 'Whyalla', 'Lismore', 'Orange', 'Dubbo', 'Bathurst', 'Coffs Harbour', 'Mildura', 'Bairnsdale', 'Wangaratta', 'Murray Bridge', 'Goulburn', 'Coffs Harbour', 'Port Lincoln', 'Burnie-Devonport', 'Launceston', 'Alice Springs', 'Broome', 'Karratha', 'Port Hedland', 'Geraldton', 'Kalgoorlie-Boulder', 'Mount Isa', 'Mackay', 'Rockhampton', 'Bundaberg', 'Hervey Bay', 'Maryborough', 'Gympie', 'Noosa Heads', 'Sunshine Coast'],
+        },
+        { name: 'India', language:'hi', value: 'IN', flag: 'https://flagcdn.com/16x12/in.png' ,
+          states: ['Maharashtra', 'Karnataka', 'Tamil Nadu', 'West Bengal'],
+          cities: ['Mumbai', 'Bangalore', 'Chennai', 'Kolkata', 'Hyderabad', 'Ahmedabad', 'Pune', 'Surat', 'Jaipur', 'Lucknow', 'Kanpur', 'Nagpur', 'Indore', 'Thane', 'Bhopal', 'Visakhapatnam', 'Patna', 'Vadodara', 'Ghaziabad', 'Ludhiana', 'Agra', 'Nashik', 'Faridabad', 'Meerut', 'Rajkot', 'Kalyan-Dombivli', 'Vasai-Virar', 'Varanasi', 'Srinagar', 'Aurangabad', 'Dhanbad', 'Amritsar', 'Navi Mumbai', 'Allahabad', 'Ranchi', 'Howrah', 'Coimbatore', 'Jabalpur', 'Gwalior', 'Vijayawada', 'Jodhpur', 'Madurai', 'Raipur', 'Kota', 'Chandigarh', 'Solapur', 'Bareilly', 'Moradabad', 'Mysore', 'Guwahati', 'Jalandhar', 'Thiruvananthapuram', 'Bhubaneswar', 'Salem', 'Warangal', 'Tiruchirappalli', 'Aligarh', 'Jammu', 'Guntur', 'Udaipur', 'Siliguri', 'Jhansi', 'Ulhasnagar', 'Nellore', 'Jamshedpur', 'Bikaner', 'Bellary'],
+        },
+        { name: 'France', language:'fr', value: 'FR', flag: 'https://flagcdn.com/16x12/fr.png',
+          states: ['Île-de-France', 'Provence-Alpes-Côte d\'Azur', 'Auvergne-Rhône-Alpes', 'Nouvelle-Aquitaine'],
+          cities: ['Paris', 'Marseille', 'Lyon', 'Bordeaux', 'Nice', 'Toulouse', 'Nantes', 'Strasbourg', 'Montpellier', 'Rennes', 'Lille', 'Reims', 'Le Havre', 'Saint-Étienne', 'Toulon', 'Grenoble', 'Dijon', 'Angers', 'Villeurbanne', 'Nîmes', 'Clermont-Ferrand', 'Aix-en-Provence', 'Brest', 'Limoges', 'Tours', 'Amiens', 'Metz', 'Perpignan', 'Besançon', 'Orléans', 'Boulogne-Billancourt', 'Mulhouse', 'Rouen', 'Caen', 'Nancy', 'Saint-Denis', 'Argenteuil', 'Montreuil', 'Avignon', 'Asnières-sur-Seine', 'Colombes', 'Aubervilliers', 'Rueil-Malmaison', 'Le Tampon', 'Saint-Paul', 'Saint-Pierre', 'Fort-de-France', 'Pointe-à-Pitre', 'Saint-Denis', 'Le Port', 'Saint-André', 'Saint-Louis', 'Sainte-Marie', 'Sainte-Suzanne', 'Le Lamentin', 'La Rochelle', 'Saint-Nazaire', 'Chambéry', 'Annecy', 'Agen', 'Alès', 'Albi', 'Aubenas'],
+         },
     ]
 
-    const handleSelect = (country) => {
+    const handleSelectCountry = (country) => {
         setSelectedCountry(country);
         setIsOpen(false);
     }
+    const handleSelectState = (state) => {
+        setselectedState(state);
+        setIsOpenState(false);
+    }
     
+    const handleSelectCity = (city) => {
+        setSelectedCity(city);
+        setIsOpenCity(false);
+        
+    } 
 
   return (
     <Container>
@@ -65,8 +110,8 @@ const CheckOutPage = () => {
           </div>
         </div>
       </div>
-        <div className='flex'>
-          <div className='w-[70%]'>
+        <div className='flex justify-between'>
+          <div className='w-[69%]'>
             <h4 className='font-["Poppins"] font-bold text-4xl leading-9 mb-10'>Billing Details</h4>
             <div className=' flex flex-wrap gap-x-4 gap-y-8 justify-between'>
               <div className='w-[49%]'>
@@ -86,44 +131,116 @@ const CheckOutPage = () => {
               </div>
               <div className='w-[49%]'>
                     <h4 className='font-["Poppins"] text-xl font-bold text-[#303030] leading-[30px] mb-3'>Country <span className='text-red-600'>*</span></h4>
-                  <div className='w-full p-2 cursor-pointer flex items-center '
+                  <div className='w-full px-[30px] py-[25px] border border-solid border-[#CBCBCB] rounded-[10px] cursor-pointer flex items-center  '
                     onClick={() => setIsOpen(!isOpen)}
                     >
                         {selectedCountry ? (
                             <>
-                                <img src={selectedCountry.flag} alt={selectedCountry.name} className='w-4 h-4 mr-4' />
-                               <span className='mr-2'>
+                                
+                               <span className='w-[241px] flex items-center gap-2 text-[#646464]'>
                                  {selectedCountry.name}
                                 </span>
-                                <FaAngleDown />
+                                <FaAngleDown className='ml-[241px] text-[#646464]'/>
                             </>
                         ) : (
-                            <span className='mr-2'>Country<FaAngleDown /></span>
+                            <span className='mr-2 '>Country<FaAngleDown /></span>
                         )}
                     </div>
             
                     {isOpen && (
-                        <ul className='absolute w-full border-gray-300 bg-white shadow-lg z-10 mt-1'>
+                        <ul className='absolute w-[520px] border-gray-300 bg-white shadow-lg z-10 mt-1'>
                             {countries.map((country) => (
                             <li
                             key={country.value} 
-                             className='flex items-center gap-2 p-2 cursor-pointer hover:bg-gray-200'
-                            onClick={() =>handleSelect(country)}
+                             className='flex items-center gap-2 px-7 cursor-pointer  text-[#646464] py-3 hover:bg-gray-200'
+                            onClick={() =>handleSelectCountry(country)}
                              >
-            
-                                <img src={country?.flag} alt={country?.name} />
                                 {country?.name}
                             </li>
                         ))}
             
                         </ul>)}
               </div>
+              <div className='w-[49%]'>
+                    <h4 className='font-["Poppins"] text-xl font-bold text-[#303030] leading-[30px] mb-3'>State <span className='text-red-600'>*</span></h4>
+                  <div className='w-full px-[30px] py-[25px] border border-solid border-[#CBCBCB] rounded-[10px] cursor-pointer flex items-center  '
+                    onClick={() => setIsOpenState(!isOpenState)}
+                    >
+                        {selectedState ? (
+                            <>
+                                
+                               <span className='w-[241px] flex items-center gap-2 text-[#646464]'>
+                                 {selectedState}
+                                </span>
+                                <FaAngleDown className='ml-[241px] text-[#646464]'/>
+                            </>
+                        ) : (
+                            <span className='mr-2 '>Select State<FaAngleDown /></span>
+                        )}
+                    </div>
             
+                    {isOpenState  && selectedCountry&& (
+                        <ul className='absolute w-[520px] border-gray-300 bg-white shadow-lg z-10 mt-1'>
+                            {selectedCountry.states.map((state, index) => (
+                            <li
+                            key={state} 
+                             className='flex items-center gap-2 px-7 cursor-pointer  text-[#646464] py-3 hover:bg-gray-200'
+                            onClick={() =>handleSelectState(state)}
+                             >
+                                {state}
+                            </li>
+                        ))}
+            
+                        </ul>)}
+              </div>
+              <div className='w-[49%]'>
+                <h4 className='font-["Poppins"] text-xl font-bold text-[#303030] leading-[30px] mb-3'>City <span className='text-red-600'>*</span></h4>
+                <div className='w-full px-[30px] py-[25px] border border-solid border-[#CBCBCB] rounded-[10px] cursor-pointer flex items-center  '
+                onClick={() => setIsOpenCity(!isOpenCity)}
+                > 
+                  {selectedCity ? (
+                    <>
+                                  
+                      <span className='w-[241px] flex items-center gap-2 text-[#646464]'>
+                        {selectedCity}
+                      </span>
+                      <FaAngleDown className='ml-[241px] text-[#646464]'/>
+                    </>
+                  ) : (
+                    <span className='mr-2 '>Select City<FaAngleDown /></span>
+                  )}
+                </div>
+            
+                {isOpenCity && selectedState && selectedCountry && Array.isArray(selectedCountry.cities) && (
+                  <ul className='absolute w-[520px] border-gray-300 bg-white shadow-lg z-10 mt-1'>
+                    {selectedCountry.cities.map((city) => (
+                      <li
+                      key={city}
+                      className='flex items-center gap-2 px-7 cursor-pointer text-[#646464] py-3 hover:bg-gray-200'
+                      onClick={() => {handleSelectCity(city)}}
+                      >
+                        {city}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+
+              <div className='w-[49%]'>
+                <Box inputName={"ZIP Code"} placeholder={"555"}/>
+              </div>            
+
+
+              <div className='w-full'>
+                <Box inputName={"Order Notes"} placeholder={"Enter your order notes ..."} type='textarea'/>
+              </div>
             </div>
                   
           </div>
-          <div className='w-[30%] bg-[#F5F5F5] rounded-[10px] h-10'>
-            hlw
+          <div className='w-[29%]'>
+            <div className='bg-[#F4F4F4] rounded-[25px] pt-10 pb-12 '>
+              <h4 className='font-["Poppins"] font-semibold text-2xl leading-[30px] mb-12'>Order Summary</h4>
+            </div>
           </div>
 
         </div>
